65953e5f30156d2f341bddd13727aa05262322d9
Ilya Litvinov <ilyaillych@gmail.com>
2016-11-06 01:00:38 +0200

    Initialize project added infrastructure

---
+++ gulpfile.babel.js
@@ -0,0 +1,84 @@
+/**
+ * Created by IlyaLitvinov on 06.11.16.
+ */
+'use strict';
+const gulp = require('gulp');
+const stylus = require('gulp-stylus');
+const livereload = require('gulp-livereload');
+const connect = require('gulp-connect');
+const inject = require('gulp-inject');
+const clean = require('gulp-clean');
+
+const dirs = {
+    src: 'src',
+    dest: 'build'
+};
+const paths = {
+    src: `${dirs.src}/styles.styl`,
+    dest: `${dirs.dest}/styles/`,
+};
+
+
+gulp.task('styles', () => {
+    return gulp.src(paths.src)
+        .pipe(stylus())
+        .pipe(gulp.dest(paths.dest))
+        .pipe(livereload());
+});
+
+gulp.task('server', () => {
+    connect.server({
+        root: 'build',
+        port: 3001,
+        livereload: true
+    });
+});
+
+gulp.task('html', () => {
+    return gulp.src('src/index.html')
+        .pipe(gulp.dest(`${dirs.dest}/`))
+        .pipe(connect.reload());
+});
+
+gulp.task('inject', ['styles'], () => {
+    let target = gulp.src('./build/index.html');
+    let sources = gulp.src([
+        `${paths.dest}*.css`
+    ], {read: true});
+    return target.pipe(inject(sources, {
+        ignorePath: dirs.dest,
+        addRootSlash: false
+    }))
+        .pipe(gulp.dest(dirs.dest))
+        .pipe(connect.reload());
+});
+
+gulp.task('assets', () => {
+    return gulp.src(['assets/**/*.png', 'assets/**/*.jpg'])
+        .pipe(gulp.dest(`${dirs.dest}/assets`))
+        .pipe(connect.reload());
+});
+
+gulp.task('clean', () => {
+    return gulp.src([
+        `${dirs.dest}/**/*.css`,
+        `${dirs.dest}/**/*.html`
+    ], {read: false})
+        .pipe(clean());
+});
+
+gulp.task('watch', () => {
+    gulp.watch(['src/**/*.styl'], ['styles', 'inject']);
+    gulp.watch(['src/**/*.html'], ['html', 'inject']);
+});
+
+gulp.task('dev', ['clean'], () => {
+    gulp.run(
+        'server',
+        'styles',
+        'html',
+        'inject',
+        'assets',
+        'watch'
+    );
+});
